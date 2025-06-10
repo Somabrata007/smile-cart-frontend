e5cda4f9e26b65decfe7b29193a3a065bc3d65eb
Somabrata <somabratasatpathy2025@gmail.com>
2025-06-10 10:31:47 +0530

    Added Product Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import "./App.css";
-// eslint-disable-next-line import/extensions
-import logo from "./logo.svg";
+// import { Typography } from "neetoui";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img alt="logo" className="App-logo" src={logo} />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+import "./App.css";
+import Product from "./components/Product";
+// eslint-disable-next-line import/extensions
+// import logo from "./logo.svg";
+
+// const App = () => (
+//   <div className="App">
+//     <header className="App-header">
+//       <img alt="logo" className="App-logo" src={logo} />
+//       <Typography style="body1">
+//         Edit <code>src/App.js</code> and save to reload.
+//       </Typography>
+//       <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         rel="noopener noreferrer"
+//         target="_blank"
+//       >
+//         Learn React
+//       </a>
+//     </header>
+//   </div>
+// );
+
+const App = () => <Product />;
 
 export default App;
---
+++ src/components/Product.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Typography } from "neetoui";
+
+const Product = () => (
+  <Typography style="h1">This is the product page of Smile Cart.</Typography>
+);
+
+export default Product;
